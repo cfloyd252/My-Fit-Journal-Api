2b5aedb8933b558d021c86f60c8dc1be24bf1dc5
Christopher Floyd <cfloyd252@gmail.com>
2020-03-12 09:23:16 -0700

    cleaned up code

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/my-fit-journal',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/my-fit-journal-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-}
+};
