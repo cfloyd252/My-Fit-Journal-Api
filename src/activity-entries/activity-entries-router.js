b876cdee7d567884a1821d813ac20eecf8f69698
Christopher Floyd <cfloyd252@gmail.com>
2020-03-12 21:12:38 -0700

    organized activity-entries-router.js

---
+++ src/activity-entries/activity-entries-router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const ActivityEntriesService = require('./activity-entries-service');
+const path = require('path');
+const { requireAuth } = require('../../middleware/jwt-auth');
+
+const activityEntriesRouter = express.Router();
+const jsonBodyParser = express.json();
+
+activityEntriesRouter
+  .route('/')
+  .all(requireAuth)
+  .get((req, res, next) => {
+    
+  })
+  .post(jsonBodyParser, (req, res, next) => {
+
+  });
+
+module.exports = activityEntriesRouter;
