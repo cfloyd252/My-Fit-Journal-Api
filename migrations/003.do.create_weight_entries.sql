e3d105be7a944cad1773fa3c803c56f7d4b73b70
Christopher Floyd <cfloyd252@gmail.com>
2020-03-24 14:32:40 -0700

    restructed database tables

--- migrations/003.do.create_weight_entries.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE weight_entries (
-  id SERIAL PRIMARY KEY,
-  quanity DECIMAL NOT NULL,
-  unit_of_measurement TEXT NOT NULL,
-  log_time TIMESTAMPTZ DEFAULT now() NOT NULL,
-  user_id INTEGER
-    REFERENCES users(id) ON DELETE CASCADE NOT NULL
-);
