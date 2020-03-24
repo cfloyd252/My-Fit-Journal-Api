1cd26d54b2252073961ed2a5e0cf76661749c872
Christopher Floyd <cfloyd252@gmail.com>
2020-03-24 15:21:46 -0700

    combined all entry services to one file

---
+++ src/log-entries/log-entries-service.js
@@ -0,0 +1,50 @@
+const LogEntriesService = {
+  getWaterEntries(db) {
+    return db
+      .from('log_entries AS lg')
+      .select(
+        'lg.quanity',
+        'lg.unit_of_measurement',
+        'lg.start_time'
+      )
+      .where({ log_type: 'water' })
+      .orderBy('start_time', 'desc')
+  },
+
+  getWeightEntries(db) {
+    return db
+      .from('log_entries AS lg')
+      .select(
+        'lg.quanity',
+        'lg.unit_of_measurement',
+        'lg.start_time'
+      )
+      .where({ log_type: 'weight' })
+      .orderBy('start_time', 'desc')
+  },
+
+  getActivityEntries(db) {
+    return db
+      .from('log_entries AS lg')
+      .select(
+        'lg.log_title',
+        'lg.start_time',
+        'lg.end_time',
+        'lg.calories'
+      )
+      .where({ log_type: 'activity' })
+      .orderBy('start_time', 'desc')
+  },
+
+  insertEntry(db, newEntry) {
+    return db
+      .insert(newEntry)
+      .into('log_entries')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
+}
+
+module.exports = LogEntriesService
